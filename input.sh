a1936edbe6d2e5ee1b73e923e56e5419e1c2ffae
Echo-pieter <75295681+Echo-pieter@users.noreply.github.com>
2024-02-02 06:54:12 +0200

    Add files via upload

---
+++ input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "what is the year"
+
+read year
+
+echo "the year is $year"
---
+++ myscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# this is a comment so the terminal will ignore this
+
+echo "hello world"
